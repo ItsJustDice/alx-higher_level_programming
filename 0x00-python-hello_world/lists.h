f382e117f8c176c16c4d969e9d74301ed9f44f29
Itsjustdice <ckdebruin@gmail.com>
2023-06-05 08:49:10 -0700

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -15,9 +15,12 @@ typedef struct listint_s
 	struct listint_s *next; /* pointer to following node in list */
 } listint_t; /* alternative name for structs singly linked list */
 
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h); /* prints elements of linked list */
+/* adds new node at start of linked list */
 listint_t *add_nodeint(listint_t **head, const int n);
+/* sets head of linked list to NULL to free it */
 int check_cycle(listint_t *list);
+/* yields 0 or 1 depending on if it finds a cycle in a linked list */
 void free_listint(listint_t *head);
 
 #endif /* LISTS.H */
