2a931573527ab4fbe65cb8741cffcf0cd0a4110f
Itsjustdice <ckdebruin@gmail.com>
2023-06-05 11:53:53 -0700

    7-edges.py

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+word = "Hello"
+word_first_3 = word[:3]
+word_first_2 = word[-2:]
+middle_word = word[1:-1]
+
+print("First 3 letters: {}". format(word_first_3))
+print("Last_2_letters: {}". format(word_last_2))
+print("Middle_Word: {}". format(middle_word))
