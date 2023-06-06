4d60c80a36a340d05f029f2c102a56ef597c114e
Itsjustdice <ckdebruin@gmail.com>
2023-06-06 15:56:48 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-j = 0
-for b in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(b - j)), end="")
-    j = 32 if j == 0 else 0
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
