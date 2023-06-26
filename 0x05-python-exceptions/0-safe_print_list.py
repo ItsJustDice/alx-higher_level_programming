cb99fc5bfefa74458d0a80749c0654caa21f82a0
ItsJustDice <ckdebruin@gmail.com>
2023-06-26 09:07:06 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    matrix = 0
+    for j in range(x):
+        try:
+            print(f"{my_list[j]}", end="")
+            matrix += 1
+        except IndexError:
+            break
+    print()
+    return(matrix)
