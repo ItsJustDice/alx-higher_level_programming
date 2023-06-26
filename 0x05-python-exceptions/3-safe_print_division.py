a292db636fa666f08335fafe7028a85e1a6926b6
ItsJustDice <ckdebruin@gmail.com>
2023-06-26 10:38:13 +0000

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        count = a / b
-        print("Inside count: {:.1f}".format(count))
-    except:
-        count = None
-        print("Inside count: {}".format(count))
+        div = a / b
+    except ZeroDivisionError:
+        div = None
     finally:
-        return count
+        print("Inside result: {}".format(div))
+    return div
