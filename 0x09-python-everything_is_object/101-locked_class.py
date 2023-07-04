17d35c750f6cc4160d4a152abfe519b1193c641c
ItsJustDice <ckdebruin@gmail.com>
2023-07-04 14:43:53 +0000

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a defined locked class"""
+
+
+class LockedClass:
+    """
+    enables the establishment of the first_name attribute
+    """
+
+    __slots__ = ["first_name"]
