cefed03731257177aead1a1010213af4d34219a9
Candice <122986059+ItsJustDice@users.noreply.github.com>
2024-01-03 20:34:40 +0200

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Get the API URL from the command line argument
+const apiUrl = process.argv[2];
+
+// Make a GET request to the specified API endpoint
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    // Parse the JSON response and extract movie results
+    const results = JSON.parse(body).results;
+
+    // Count movies where character "Wedge Antilles" is present
+    const moviesWithWedge = results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0);
+
+    // Print the count of movies
+    console.log(moviesWithWedge);
+  }
+});
